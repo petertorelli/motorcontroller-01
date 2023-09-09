21ed40052a3d6ec3382afee96138e861778ed089
Peter Torelli <peter.j.torelli@gmail.com>
2023-09-09 11:47:09 -0700

    ready for integration

--- Core/app.c
+++ Core/app.c
@@ -44,6 +44,16 @@
  * there will not be any overflow/underflow of the encoder at 10ms for these
  * motors. (Changing the motors or sample rate will impact this.)
  *
+ *
+ * Concerns:
+ *
+ * - PID filter "I" mode is laggy. Needs better tuning.
+ *
+ * - The direction of the INA/B motor controllers can change before the pid has a chance
+ *   to respond. This means if we go from 100.0 to -1.0 it will immediately switch to
+ *   -100.0 until the PID can respond. We should probably use the quadrature to set the
+ *   direction control bits and not the requested sign. This discontinuity should be
+ *   checked for ... somewhere?
  */
 
 #include "app.h"
@@ -52,7 +62,6 @@ extern I2C_HandleTypeDef SERVER_I2C;
 
 extern TIM_HandleTypeDef M1_ENC_TIM;
 extern TIM_HandleTypeDef M1_PWM_TIM;
-
 extern TIM_HandleTypeDef M2_ENC_TIM;
 extern TIM_HandleTypeDef M2_PWM_TIM;
 
@@ -60,10 +69,14 @@ extern TIM_HandleTypeDef M2_PWM_TIM;
 #define RX_BUFFER_SIZE         4u
 #define TIM_CTR_PERIOD         0x10000u
 #define CTR_RESET              (TIM_CTR_PERIOD >> 1)
-#define MIN_PWM                550u
+#define MIN_PWM                0u
 #define MAX_PWM                1000u
 #define MIN_SPEED              -1000
 #define MAX_SPEED              1000
+#define MAX_PPR                240u
+
+#define NMOTOR_1 0u
+#define NMOTOR_2 1u
 
 static volatile bool g_i2c_data                  = false;
 uint8_t              g_rx_buffer[RX_BUFFER_SIZE] = { 0u, 0u, 0u, 0u };
@@ -90,14 +103,16 @@ HAL_I2C_SlaveRxCpltCallback(I2C_HandleTypeDef *hi2c)
     }
 }
 
-/**
- * Probably a good idea to stop all the motors if something goes wrong?
- * At least put them in "no drive" mode so they just spin.
- */
 static void
 hard_shutdown(void)
 {
     printf("hard_shutdown(): now\n");
+	M1_PWM_TIM.Instance->M1_CCR = 0;
+	HAL_GPIO_WritePin(g_a_ports[0], g_a_pins[0], GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(g_b_ports[0], g_b_pins[0], GPIO_PIN_RESET);
+	M2_PWM_TIM.Instance->M2_CCR = 0;
+	HAL_GPIO_WritePin(g_a_ports[1], g_a_pins[1], GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(g_b_ports[1], g_b_pins[1], GPIO_PIN_RESET);
 }
 /**
  * Change the target setpoint for the motor's PID.
@@ -106,7 +121,7 @@ hard_shutdown(void)
  * TODO: should we handle radical changes in direction / magnitude?
  */
 static void
-set_motor(unsigned nmotor, int16_t speed_dir)
+set_motor(unsigned mask, int16_t speed_dir)
 {
     GPIO_PinState a;
     GPIO_PinState b;
@@ -124,42 +139,63 @@ set_motor(unsigned nmotor, int16_t speed_dir)
         b = GPIO_PIN_SET;
     }
 
-    HAL_GPIO_WritePin(g_a_ports[nmotor - 1], g_a_pins[nmotor - 1], a);
-    HAL_GPIO_WritePin(g_b_ports[nmotor - 1], g_b_pins[nmotor - 1], b);
-
     // Don't scale out of zero.
     if (speed_dir == 0)
     {
         t = 0.0f;
+        if (mask & 1)
+        {
+			M1_PWM_TIM.Instance->M1_CCR = 0;
+			HAL_GPIO_WritePin(g_a_ports[0], g_a_pins[0], GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(g_b_ports[0], g_b_pins[0], GPIO_PIN_RESET);
+        }
+        if (mask & 2)
+        {
+			M2_PWM_TIM.Instance->M2_CCR = 0;
+			HAL_GPIO_WritePin(g_a_ports[1], g_a_pins[1], GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(g_b_ports[1], g_b_pins[1], GPIO_PIN_RESET);
+        }
     }
     else
     {
-        // Scale [0,1000] to [550,900]
         t = abs(speed_dir);
-        t = (t / TIM_CTR_PERIOD) * (MAX_PWM - MIN_PWM) + MIN_PWM;
+        t = (t / MAX_SPEED) * MAX_PPR;
     }
+    // TODO: Changing direction before setting the PID is discontinuous
+    if (mask & 1)
+    {
+    	g_pids[0].target = t;
+        HAL_GPIO_WritePin(g_a_ports[0], g_a_pins[0], a);
+        HAL_GPIO_WritePin(g_b_ports[0], g_b_pins[0], b);
 
-    g_pids[nmotor - 1].target = t;
+    }
+    if (mask & 2)
+    {
+    	g_pids[1].target = t;
+        HAL_GPIO_WritePin(g_a_ports[1], g_a_pins[1], a);
+        HAL_GPIO_WritePin(g_b_ports[1], g_b_pins[1], b);
+
+    }
 }
 
 static void
-process_command(void)
+parse_i2c(void)
 {
     uint8_t command   = g_rx_buffer[0];
-    uint8_t nmotor    = g_rx_buffer[1];
+    uint8_t mask      = g_rx_buffer[1];
     int16_t speed_dir = (g_rx_buffer[3] << 8) | g_rx_buffer[2];
 
-    if ((command != STANDARD_MOTOR_COMMAND) || (nmotor < 1 || nmotor > 2)
+    if ((command != STANDARD_MOTOR_COMMAND) || (mask < 1 || mask > 3)
         || (speed_dir < MIN_SPEED || speed_dir > MAX_SPEED))
     {
-        printf("Invalid packet: 0x%02x %d -> %d\n", command, nmotor, speed_dir);
-        // hard_shutdown();
-        // Error_Handler();
+        printf("parse_i2c: ERROR 0x%02x %d %d\n", command, mask, speed_dir);
+        hard_shutdown();
+        Error_Handler();
     }
     else
     {
-        printf("Setting motor %d to %d / 10\n", nmotor, speed_dir);
-        set_motor(nmotor, speed_dir);
+        printf("parse_i2c: Set mask %d to %d / 10\n", mask, speed_dir);
+        set_motor(mask, speed_dir);
     }
 }
 
@@ -174,7 +210,7 @@ app_init(void)
     stat = HAL_I2C_Slave_Receive_IT(&SERVER_I2C, g_rx_buffer, RX_BUFFER_SIZE);
     if (stat != HAL_OK)
     {
-        printf("ERROR: Failed to set initial receive: 0x%02x\n", stat);
+        printf("app_init: ERROR Failed to set receive: 0x%02x\n", stat);
         Error_Handler();
     }
 
@@ -182,14 +218,14 @@ app_init(void)
 
     g_pids[0].target   = 0.0f;
     g_pids[0].kp       = 0.0f;
-    g_pids[0].ki       = 4.0f;
+    g_pids[0].ki       = 1.0f;
     g_pids[0].kd       = 0.0f;
     g_pids[0].ymin     = (float)MIN_PWM;
     g_pids[0].ymax     = (float)MAX_PWM;
     g_pids[0].period_s = 0.10f;
 
     memcpy(&(g_pids[1]), &(g_pids[0]), sizeof(pidctl_t));
-#if 1
+
     printf("app_init: start encoders\n");
     M1_ENC_TIM.Instance->CNT = CTR_RESET;
     M2_ENC_TIM.Instance->CNT = CTR_RESET;
@@ -199,16 +235,16 @@ app_init(void)
 
     printf("app_init: setting motors to free-spin\n");
     HAL_GPIO_WritePin(g_a_ports[0], g_a_pins[0], GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(g_b_ports[0], g_b_pins[0], GPIO_PIN_SET);
+    HAL_GPIO_WritePin(g_b_ports[0], g_b_pins[0], GPIO_PIN_RESET);
     HAL_GPIO_WritePin(g_a_ports[1], g_a_pins[1], GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(g_b_ports[1], g_b_pins[1], GPIO_PIN_SET);
+    HAL_GPIO_WritePin(g_b_ports[1], g_b_pins[1], GPIO_PIN_RESET);
 
     printf("app_init: start PWMs\n");
     M1_PWM_TIM.Instance->M1_CCR = 0;
     M2_PWM_TIM.Instance->M2_CCR = 0;
     HAL_TIM_PWM_Start(&M1_PWM_TIM, M1_PWM_CH);
     HAL_TIM_PWM_Start(&M2_PWM_TIM, M2_PWM_CH);
-#endif
+
     printf("app_init: end\n");
 }
 
@@ -224,7 +260,6 @@ update_pids(void)
         // Get our 'y' values...
         m1y = pidctl_compute(&(g_pids[0]), m1x);
         m2y = pidctl_compute(&(g_pids[1]), m2x);
-        printf("%lu : M1 count = %f, M2 count = %f\n", HAL_GetTick(), m1x, m2x);
         // Update the PWM CCRs...
         M1_PWM_TIM.Instance->M1_CCR = (uint32_t)m1y;
         M2_PWM_TIM.Instance->M2_CCR = (uint32_t)m2y;
@@ -242,12 +277,12 @@ app_loop(void)
     if (g_i2c_data == true)
     {
         g_i2c_data = false;
-        process_command();
+        parse_i2c();
         stat = HAL_I2C_Slave_Receive_IT(
             &SERVER_I2C, g_rx_buffer, RX_BUFFER_SIZE);
         if (stat != HAL_OK)
         {
-            printf("ERROR: Failed subsequent receive: 0x%02x\n", stat);
+            printf("app_loop: ERROR: Failed to set receive: 0x%02x\n", stat);
             hard_shutdown();
             Error_Handler();
         }
